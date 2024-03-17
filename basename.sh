6600ebf759828e07d864c44d010c836fca933e40
Stefanie-A <albertstephanie630@gmail.com>
2024-03-16 19:12:05 -0700

    Modified

--- basename.sh
+++ basename.sh
@@ -1,9 +1,17 @@
 #!bin/bash
 
 #Changing file(s) extensions
+directory = '/path/directory'
 
-for file in *.HTML; do
-    name = $(basename "$file" .HTML)
-    mv "$file" "$name.txt"
+for files in $directory/*HTML; do
+
+    name = $(basename "$files" .HTML)
+
+    new_files = "$directory/$name.txt"
+
+    mv "$file" "$new_files"
 
 done
+
+echo "Files renamed"
+
